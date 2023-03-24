bbfc2b540000155d2931f2e3c599ece4f4e2f83e
AleksanderOMD <alexvalkoinenomd@gmail.com>
2023-03-23 19:41:05 -0500

    Initial Beta release

---
+++ Rocksmith DLC mover/CDLC Renamer.Designer.cs
@@ -0,0 +1,207 @@
+﻿
+namespace Rocksmith_DLC_mover
+{
+    partial class CDLCReNamer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusBox = new System.Windows.Forms.RichTextBox();
+            this.btnSetDownloadFolder = new System.Windows.Forms.Button();
+            this.BtnSetRSFolder = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnAbort = new System.Windows.Forms.Button();
+            this.btnTransfer = new System.Windows.Forms.Button();
+            this.btnCleanup = new System.Windows.Forms.Button();
+            this.gbOptions = new System.Windows.Forms.GroupBox();
+            this.cbAutoSort = new System.Windows.Forms.CheckBox();
+            this.cbSaveOrig = new System.Windows.Forms.CheckBox();
+            this.btnAuto = new System.Windows.Forms.Button();
+            this.lblAuto = new System.Windows.Forms.Label();
+            this.gbOptions.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusBox
+            // 
+            this.statusBox.Enabled = false;
+            this.statusBox.Location = new System.Drawing.Point(13, 13);
+            this.statusBox.Name = "statusBox";
+            this.statusBox.ReadOnly = true;
+            this.statusBox.Size = new System.Drawing.Size(557, 617);
+            this.statusBox.TabIndex = 0;
+            this.statusBox.Text = "";
+            // 
+            // btnSetDownloadFolder
+            // 
+            this.btnSetDownloadFolder.Location = new System.Drawing.Point(595, 13);
+            this.btnSetDownloadFolder.Name = "btnSetDownloadFolder";
+            this.btnSetDownloadFolder.Size = new System.Drawing.Size(187, 49);
+            this.btnSetDownloadFolder.TabIndex = 1;
+            this.btnSetDownloadFolder.Text = "Set Download Folder";
+            this.btnSetDownloadFolder.UseVisualStyleBackColor = true;
+            this.btnSetDownloadFolder.Click += new System.EventHandler(this.btnSetDownloadFolder_Click);
+            // 
+            // BtnSetRSFolder
+            // 
+            this.BtnSetRSFolder.Location = new System.Drawing.Point(595, 68);
+            this.BtnSetRSFolder.Name = "BtnSetRSFolder";
+            this.BtnSetRSFolder.Size = new System.Drawing.Size(187, 48);
+            this.BtnSetRSFolder.TabIndex = 2;
+            this.BtnSetRSFolder.Text = "Set RS Folder";
+            this.BtnSetRSFolder.UseVisualStyleBackColor = true;
+            this.BtnSetRSFolder.Click += new System.EventHandler(this.BtnSetRSFolder_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(595, 686);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(187, 50);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnAbort
+            // 
+            this.btnAbort.Location = new System.Drawing.Point(13, 677);
+            this.btnAbort.Name = "btnAbort";
+            this.btnAbort.Size = new System.Drawing.Size(187, 59);
+            this.btnAbort.TabIndex = 4;
+            this.btnAbort.Text = "Abort Transfer";
+            this.btnAbort.UseVisualStyleBackColor = true;
+            // 
+            // btnTransfer
+            // 
+            this.btnTransfer.Location = new System.Drawing.Point(383, 677);
+            this.btnTransfer.Name = "btnTransfer";
+            this.btnTransfer.Size = new System.Drawing.Size(187, 59);
+            this.btnTransfer.TabIndex = 5;
+            this.btnTransfer.Text = "Start Transfer";
+            this.btnTransfer.UseVisualStyleBackColor = true;
+            this.btnTransfer.Click += new System.EventHandler(this.btnTransfer_Click);
+            // 
+            // btnCleanup
+            // 
+            this.btnCleanup.Location = new System.Drawing.Point(595, 164);
+            this.btnCleanup.Name = "btnCleanup";
+            this.btnCleanup.Size = new System.Drawing.Size(187, 48);
+            this.btnCleanup.TabIndex = 6;
+            this.btnCleanup.Text = "Cleanup CDLC folder";
+            this.btnCleanup.UseVisualStyleBackColor = true;
+            this.btnCleanup.Click += new System.EventHandler(this.btnCleanup_Click);
+            // 
+            // gbOptions
+            // 
+            this.gbOptions.Controls.Add(this.cbAutoSort);
+            this.gbOptions.Controls.Add(this.cbSaveOrig);
+            this.gbOptions.Location = new System.Drawing.Point(595, 388);
+            this.gbOptions.Name = "gbOptions";
+            this.gbOptions.Size = new System.Drawing.Size(200, 170);
+            this.gbOptions.TabIndex = 7;
+            this.gbOptions.TabStop = false;
+            this.gbOptions.Text = "Options";
+            // 
+            // cbAutoSort
+            // 
+            this.cbAutoSort.AutoSize = true;
+            this.cbAutoSort.Location = new System.Drawing.Point(7, 80);
+            this.cbAutoSort.Name = "cbAutoSort";
+            this.cbAutoSort.Size = new System.Drawing.Size(152, 24);
+            this.cbAutoSort.TabIndex = 1;
+            this.cbAutoSort.Text = "Auto Sort on Xfer";
+            this.cbAutoSort.UseVisualStyleBackColor = true;
+            this.cbAutoSort.CheckedChanged += new System.EventHandler(this.cbAutoSort_CheckedChanged);
+            // 
+            // cbSaveOrig
+            // 
+            this.cbSaveOrig.AutoSize = true;
+            this.cbSaveOrig.Location = new System.Drawing.Point(7, 40);
+            this.cbSaveOrig.Name = "cbSaveOrig";
+            this.cbSaveOrig.Size = new System.Drawing.Size(158, 24);
+            this.cbSaveOrig.TabIndex = 0;
+            this.cbSaveOrig.Text = "Save Original Files";
+            this.cbSaveOrig.UseVisualStyleBackColor = true;
+            this.cbSaveOrig.CheckedChanged += new System.EventHandler(this.cbSaveOrig_CheckedChanged);
+            // 
+            // btnAuto
+            // 
+            this.btnAuto.Location = new System.Drawing.Point(595, 582);
+            this.btnAuto.Name = "btnAuto";
+            this.btnAuto.Size = new System.Drawing.Size(187, 48);
+            this.btnAuto.TabIndex = 8;
+            this.btnAuto.Text = "Auto Mode";
+            this.btnAuto.UseVisualStyleBackColor = true;
+            this.btnAuto.Click += new System.EventHandler(this.btnAuto_Click);
+            // 
+            // lblAuto
+            // 
+            this.lblAuto.Location = new System.Drawing.Point(595, 637);
+            this.lblAuto.Name = "lblAuto";
+            this.lblAuto.Size = new System.Drawing.Size(187, 34);
+            this.lblAuto.TabIndex = 9;
+            // 
+            // CDLCReNamer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(794, 748);
+            this.Controls.Add(this.lblAuto);
+            this.Controls.Add(this.btnAuto);
+            this.Controls.Add(this.gbOptions);
+            this.Controls.Add(this.btnCleanup);
+            this.Controls.Add(this.btnTransfer);
+            this.Controls.Add(this.btnAbort);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.BtnSetRSFolder);
+            this.Controls.Add(this.btnSetDownloadFolder);
+            this.Controls.Add(this.statusBox);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "CDLCReNamer";
+            this.Text = "CDLC Renamer";
+            this.gbOptions.ResumeLayout(false);
+            this.gbOptions.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox statusBox;
+        private System.Windows.Forms.Button btnSetDownloadFolder;
+        private System.Windows.Forms.Button BtnSetRSFolder;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnAbort;
+        private System.Windows.Forms.Button btnTransfer;
+        private System.Windows.Forms.Button btnCleanup;
+        private System.Windows.Forms.GroupBox gbOptions;
+        private System.Windows.Forms.CheckBox cbAutoSort;
+        private System.Windows.Forms.CheckBox cbSaveOrig;
+        private System.Windows.Forms.Button btnAuto;
+        private System.Windows.Forms.Label lblAuto;
+    }
+}
+
---
+++ Rocksmith DLC mover/CDLC Renamer.cs
@@ -0,0 +1,419 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+using System.Windows.Forms;
+using System.Drawing;
+//using System.Threading;
+
+namespace Rocksmith_DLC_mover
+{
+    public partial class CDLCReNamer : Form
+    {
+        //setting appdata path as null so in the settings function I can do a nullcheck to falesafe it.
+        string AppDataPath = null;
+        string configPath = null;
+        string dlcFolder = null;
+        string cdlcFolder = null;
+        public static bool Auto = false;
+        //DLC append
+        string dlcAppend = "\\dlc\\cdlc";
+        public static string[] settings = new string[] { "", "", "0", "0" };
+
+        public static FileSystemWatcher fs = new FileSystemWatcher();
+        public CDLCReNamer()
+        {
+            InitializeComponent();
+            btnTransfer.Enabled = false;
+            checkForSettings();
+            lblAuto.BackColor = Color.Red;
+        }
+        private void setLabel()
+        {
+            if (Auto)
+            {
+                lblAuto.BackColor = Color.Green;
+            }
+            else
+            {
+                lblAuto.BackColor = Color.Red;
+            }
+        }
+        private void btnSetDownloadFolder_Click(object sender, EventArgs e)
+        {
+            //first we need to create a folder browser element. That's easy, so we're going to set that as Variable Dialog.
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            if (dialog.ShowDialog() == DialogResult.OK) //This shows the dialog, then if the result was 'ok' then it will do the following.
+            {
+                settings[0] = dialog.SelectedPath;
+                print("Download folder is now set to " + settings[0]);
+                writeSettings();
+                //downFolder = dialog.SelectedPath;
+                //statusBox.AppendText("Download folder has been set to " + downFolder);
+            }
+        }
+
+        private void BtnSetRSFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            { 
+                settings[1] = dialog.SelectedPath;
+                print("RS folder has been set to " + settings[1]);
+                writeSettings();
+            }
+        }
+
+        //We're going to check for a setting file in AppData, for out program, If they don't exist, we're going to create them.
+        private void checkForSettings()
+        {
+            //before we get started we want the local AppData path, this uses the System Namespace.
+            //This will allow us to set the destination in a nice folder there. So let's do that first.
+            string FolderPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            //Now we create a variable with the "company" name for this I'm using OMD during development, Using caps in a directory is inherently a windows thing, Linux cares more so the convention is don't us caps or space on Linux filesystems. This is a strictly windows APP so it's fine to do caps)
+            string orgFolder = FolderPath + "\\OMD";
+
+            //DEBUG
+            //print("DEBUG: AppdataPath: " + FolderPath);
+            //print("DEBUG: ORG Path: " + orgFolder);
+            //good, now we check if the path exists.
+
+            //now the try catch block to check if the file exists, If it does, return, if it doesn't, create it, if creation fails, throw exception error"
+            try
+            {
+                //Does the folder exist, Directory functions belong to Namespace System.IO
+                if (Directory.Exists(orgFolder))
+                {
+                    print("Settings Folder Exists");
+                }
+                else
+                {
+                    DirectoryInfo di = Directory.CreateDirectory(orgFolder);
+                    //print to the RTB
+                    //first get the Date/Time as string. This is a pain in the ass and requires Globalization Namespace to make it more "Friendly" but less friendly to coding, To make things easier for localization later though we SHOULD do it this way. I'm going to put the date time function in another function, then just return the result here.
+                    string Time = ConvertDate(Directory.GetCreationTime(orgFolder));
+                    string debugMessage = orgFolder + " was created at " + Time + "\n";
+                    print(debugMessage);
+                }
+            }
+            //now the catch block
+            catch (Exception e)
+            {
+                print("DEBUG EXCEPTION: " + e.ToString());
+            }
+            //Finally block in this case is empty
+            finally
+            {
+
+            }
+            AppDataPath = orgFolder;
+
+            //time not to get or create the settings file
+            //we'll set the file to config.txt remember you use a double \\ to denote a backslack in a string rather than an escape sequence
+            configPath = orgFolder + "\\config.txt";
+            //print(configPath);
+            if (File.Exists(configPath))
+            {
+                print("Config Files Exist. Attempting to restore previous settings");
+                readInSettings(configPath);
+            }
+            else
+            {
+                //file creation time. This is a bit of a weird block, the file should exist, and we aren't adding anything to it which means empty block braces.
+                using (File.Create(configPath))
+                {
+
+                }
+                print("Please set Directories to use before continueing");
+            }
+
+        }
+        public string ConvertDate(DateTime dateTime)
+        {
+            string dateString;
+            string Format = "d"; //this allows the standard US datetime specifier
+            CultureInfo culture = CultureInfo.CreateSpecificCulture("en-US"); //This creates the cultural format to use. This can be expanded to an array later for localization.
+            dateString = dateTime.ToString(Format, culture);
+            return dateString;
+        }
+        private void readInSettings(string filePath)
+        {
+            //read in the file by each line, line 0 will be the Download path, line 1 will be the RS path, it will add them to the settings list.
+            int c = 0;
+            foreach (string line in File.ReadLines(filePath))
+            {
+                if (c < 4)
+                {
+                    settings[c] = line;
+                }
+                else
+                {
+                    print("Hmm It seems there are more than 4 lines in settings, that shouldn't happen. Please check into that.");
+                }
+                c++;
+
+            }
+            foreach (string s in settings)
+            {
+                //print(s);
+            }
+            if (settings[0] == "")
+            {
+                print("The Download Directory is not set");
+            }
+            if (settings[1] == "" || settings[1] == null)
+            {
+                print("The RS Directory is not set.");
+            }
+            //print(settings[2]);
+            //print(settings[3]);
+            cbSaveOrig.Checked = settings[2] == "1" ? true : false;
+            cbAutoSort.Checked = settings[3] == "1" ? true : false;
+            
+
+            
+            checkDlcDirectories();
+            checkReady();
+        }
+
+        private void writeSettings()
+        {
+            if (configPath != null)
+            {
+                using (TextWriter tw = new StreamWriter(configPath))
+                {
+                    tw.WriteLine(settings[0]);
+                    tw.WriteLine(settings[1]);
+                    tw.WriteLine(settings[2]);
+                    tw.WriteLine(settings[3]);
+                    //print(settings[2]);
+                    //print(settings[3]);
+                    print("DEBUG: Configuration updated");
+                }
+                //while we are here since this is called any time the directories are set, this is a good time to check if the DLC folder exists, if it does, does a CDLC folder exist inside of it.
+
+            }
+            else
+            {
+                print("DEBUG: CONFIG PATH IS NULL");
+            }
+            if (settings[0] != "" && settings[1] != "")
+            {
+                btnTransfer.Enabled = true;
+            }
+            checkReady();
+        }
+
+        private void checkDlcDirectories()
+        {
+            if (settings[1] != "")
+            {
+                dlcFolder = settings[1] + "\\dlc";
+                cdlcFolder = settings[1] + dlcAppend;
+                if (Directory.Exists(dlcFolder))
+                {
+                    print("DLC folder has been located. Checking for cdlc folder.");
+                    if (Directory.Exists(cdlcFolder))
+                    {
+                        print("cdlc folder has been located.");
+                    }
+                    else
+                    {
+                        DirectoryInfo di = Directory.CreateDirectory(settings[1] + dlcAppend);
+                        string Time = ConvertDate(Directory.GetCreationTime(settings[1] + dlcAppend));
+                        string debugMessage = "DEBUG: " + settings[1] + dlcAppend + " was created at " + Time + "\n";
+                        print(debugMessage);
+                    }
+                }
+                else
+                {
+                    print("HOLDUP: Check your RS Directory, the DLC folder could not be found.");
+                    print("This folder is created by default in a RS install. Either the directory is incorrect, or the installation may be damaged");
+                }
+            }
+            else
+            {
+                print("RS Directory not set");
+            }
+        }
+
+        private void checkReady()
+        {
+            if (settings[0] != "" && settings[1] != "")
+            {
+                if (Directory.Exists(cdlcFolder))
+                {
+                    btnTransfer.Enabled = true;
+                }
+            }
+        }
+
+        private void prepareTransfer()
+        {
+            string ext = "*.psarc";
+            try
+            {
+                //var files = Directory.EnumerateFiles(settings[0], ext, SearchOption.AllDirectories);
+                string[] files = Directory.GetFiles(settings[0], ext);
+                if (Path.GetPathRoot(settings[0]) == Path.GetPathRoot(settings[1]) && cbSaveOrig.Checked != true)
+                {
+                    foreach (string file in files)
+                    {
+
+                        string fileName = file.Substring(settings[0].Length + 1);
+                        print("FILENAME: " + fileName);
+                        string message = Path.Combine(settings[0], file).ToString();
+                        print("Preparing to move: " + message);
+                        Directory.Move(file, Path.Combine(cdlcFolder, fileName));
+                        print("Moved: " + message);
+                    }
+                }
+                else
+                {
+                    print("Source and Destination are on different drives, Making copies then deleting source files");
+                    foreach (string file in files)
+                    {
+
+                        string fileName = file.Substring(settings[0].Length + 1);
+                        print("FILENAME: " + fileName);
+                        string message = Path.Combine(settings[0], file).ToString();
+                        print("Preparing to move: " + message);
+                        File.Copy(Path.Combine(settings[0], fileName), Path.Combine(cdlcFolder, fileName), true);
+                        //print("copied: " + message);
+                        //verify the file now exists in destination.
+                        if (File.Exists(Path.Combine(cdlcFolder, fileName)))
+                        {
+                            print("File copied successfully, removing source");
+                            
+                        }
+                        else
+                        {
+                            print("FILE NOT COPIED, ABORTING");
+                            return;
+                        }
+                    }
+                    if (cbSaveOrig.Checked != true)
+                    {
+                        foreach (string f in files)
+                        {
+                            print("Copy complete, Deleting source files.");
+                            File.Delete(f);
+                        } 
+                    }
+                }
+
+                if (cbAutoSort.Checked)
+                {
+                    cleanup();
+                }
+
+            }
+            
+            catch (Exception e)
+            {
+                print(e.ToString());
+            }
+        }
+
+
+        private void cleanup()
+        {
+            string path = settings[1] + dlcAppend;
+            string[] files = Directory.GetFiles(path, "*psarc");
+            foreach (string f in files)
+            {
+                string fileName = f.Substring(path.Length + 1);
+                string[] split = fileName.Split('_');
+                string bandName = split[0];
+                print(bandName);
+                string bandFolder = path + "\\" + bandName;
+                if (Directory.Exists(bandFolder))
+                {
+                    Directory.Move(f, Path.Combine(bandFolder, fileName));
+                }
+                else
+                {
+                    DirectoryInfo di = Directory.CreateDirectory(Path.Combine(path, bandName));
+                    Directory.Move(f, Path.Combine(bandFolder, fileName));
+                }
+                
+
+            }
+        }
+        private void print(string message)
+        {
+            statusBox.AppendText(message + "\n");
+            statusBox.ScrollToCaret();
+        }
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnTransfer_Click(object sender, EventArgs e)
+        {
+            prepareTransfer();
+        }
+
+        private void btnCleanup_Click(object sender, EventArgs e)
+        {
+            cleanup();
+        }
+
+        private void cbSaveOrig_CheckedChanged(object sender, EventArgs e)
+        {
+            settings[2] = cbSaveOrig.Checked ? "1" : "0";
+            writeSettings();
+        }
+
+        private void cbAutoSort_CheckedChanged(object sender, EventArgs e)
+        {
+            settings[3] = cbAutoSort.Checked ? "1" : "0";
+            writeSettings();
+        }
+
+        private void btnAuto_Click(object sender, EventArgs e)
+        {
+            if (Auto)
+            {
+                Auto = false;
+                setLabel();
+            }
+            else
+            {
+                Auto = true;
+                setLabel();
+            }
+            if (Auto)
+            {
+                DoAuto();
+            }
+            else
+            {
+                fs.EnableRaisingEvents = false;
+            }
+        }
+        public void DoAuto()
+        {
+            if (settings[0] != "")
+            {
+                fs.Path = settings[0];
+                fs.IncludeSubdirectories = true;
+                fs.Filter = "*.psarc";
+                fs.Created += new FileSystemEventHandler(autoFile);
+                fs.EnableRaisingEvents = true;
+            }
+        }
+        public void autoFile(object source, FileSystemEventArgs e)
+        {
+            if (InvokeRequired)
+            {
+                BeginInvoke(new Action(() => { this.print("New Song Detected: " + e.Name); }));
+                prepareTransfer();
+            }
+
+        }
+
+
+    } 
+
+}
